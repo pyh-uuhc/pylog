e3c7e2bce14e19f3e99996504b0764a6a6f25969
youngha <pyh.uuhc@gmail.com>
2024-08-02 20:26:05 +0900

    @admin.register(Comment)

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from blog.models import Post
+from blog.models import Post, Comment
 
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
     pass
