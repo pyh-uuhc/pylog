b6f50ced7ea666a8d34855d0bea0b0b3d95b3682
youngha <pyh.uuhc@gmail.com>
2024-08-02 21:48:18 +0900

    finish 12.4

--- config/urls.py
+++ config/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from config.views import index
@@ -24,3 +26,10 @@ urlpatterns = [
     path('', index),
     path('posts/', post_list),
 ]
+
+urlpatterns += static(
+    # URL의 접두어가 MEDIA_URL일 때는 정적파일을 돌려준다
+    prefix=settings.MEDIA_URL,
+    # 돌려줄 디렉터리는 MEDIA_ROOT를 기준으로 한다
+    document_root=settings.MEDIA_ROOT,
+)
