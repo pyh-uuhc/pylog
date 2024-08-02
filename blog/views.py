8ec6f3df0ffc87a0ea071815e68e0a915e82c74d
youngha <pyh.uuhc@gmail.com>
2024-08-02 20:35:47 +0900

    update View 'post_list'

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from blog.models import Post
 
 
 def post_list(request):
-    return render(request, 'post_list.html')
+    posts = Post.objects.all()
+    context = {
+        'posts': posts,
+    }
+
+    return render(request, 'post_list.html', context)
